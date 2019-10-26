c35ee51995ffb731816ae1ed5462deb0a550410b
Sivert Kittelsen <sivertkittelsen@gmail.com>
2019-10-26 18:49:35 +0200

    node 2 receives data in synch with node 1. it got solved by putting a while in in can_received. the interupt has not been used so far

--- lab/code/node2/main.c
+++ lab/code/node2/main.c
@@ -9,11 +9,10 @@ int main(){
     can_init();
     
     USER_DATA user_data; 
+    input_com_receive_data(&user_data);
+    printf("joystick_button %d\r\n",user_data.x_analog);
 
     while(1){
-        input_com_receive_data(&user_data);
-        printf("joystick_button %d\r\n",user_data.x_analog);
-        
     }
     return 0;
 }
--- lab/code/node2/node2_CAN.c
+++ lab/code/node2/node2_CAN.c
@@ -78,35 +78,28 @@ void can_write( const MESSAGE* msg){ // still missing support for multiple buffe
 }
 
 void can_receive(MESSAGE *msg){
-    if ( (mcp_read(MCP_CANINTF) & 0x01) || received){
-    //while(received == 0){}
-        //cli();
-        //Read id
-        msg->id = mcp_read(MCP_RXB0SIDH) << 3;
-        msg->id |= mcp_read(MCP_RXB0SIDL) >> 5;
+    //if ( (mcp_read(MCP_CANINTF) & 0x01) || received){
+    while(!(mcp_read(MCP_CANINTF) & 1)){}
+    //Read id
+    msg->id = mcp_read(MCP_RXB0SIDH) << 3;
+    msg->id |= mcp_read(MCP_RXB0SIDL) >> 5;
 
-        //Read length
-        msg->length = mcp_read(MCP_RXB0DLC) & 0x0F;
+    //Read length
+    msg->length = mcp_read(MCP_RXB0DLC) & 0x0F;
 
-        //Read data
-        for (int i=0; i < msg->length; i++){
-            msg->data[i] = mcp_read(MCP_RXB0D0 + i);
-        }
-
-        //Clear CANINTF. RX0IF after reset
-        mcp_bit_modify(MCP_CANINTF, 0x01, 0);
-
-        //clear flags
-        received = 0;
-        //sei();
+    //Read data
+    for (int i=0; i < msg->length; i++){
+        msg->data[i] = mcp_read(MCP_RXB0D0 + i);
     }
+    //Clear CANINTF. RX0IF after reset
+    mcp_bit_modify(MCP_CANINTF, 0x01, 0);
+    //clear flags
+    received = 0;
 }
-
-
 
 ISR(INTR_VECT){
     received = 1;
-    mcp_bit_modify(MCP_CANINTF, 0x01, 0);
+    //mcp_bit_modify(MCP_CANINTF, 0x01, 0);
 }
 
 
